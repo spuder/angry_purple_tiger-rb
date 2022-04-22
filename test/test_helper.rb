1d0d656116013fc843ce2ff6d32273b46d9d4584
Spencer Owen <owenspencer@gmail.com>
2022-04-22 17:13:06 -0600

    rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "angry_purple_tiger"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'angry_purple_tiger'
 
-require "minitest/autorun"
+require 'minitest/autorun'
